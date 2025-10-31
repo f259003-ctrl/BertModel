93484218b83242580768e035c1dab16c83b5ae62
f259003-ctrl <f259003@cfd.nu.edu.pk>
2025-10-30 22:44:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import torch
 from transformers import BertTokenizer, BertForSequenceClassification
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import gdown
 import os
 
